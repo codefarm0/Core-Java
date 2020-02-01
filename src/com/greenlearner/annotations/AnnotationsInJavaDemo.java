25960fcc799f26cbe286c2735459169775e4b3fb
Green Learner <greenlearner01@users.noreply.github.com>
2020-02-01 17:55:46 +0530

    more codes on serialization and ppt

---
+++ src/com/greenlearner/annotations/AnnotationsInJavaDemo.java
@@ -0,0 +1,15 @@
+package com.greenlearner.annotations;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+public class AnnotationsInJavaDemo {
+    public static void main(String[] args) {
+        System.out.println(new AnnotationsInJavaDemo());
+    }
+
+    @Override
+    public String toString() {
+        return "AnnotationsInJavaDemo{}";
+    }
+}
